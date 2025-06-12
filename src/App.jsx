1770326bae03aeb558cf11acb05d8d071beb9539
kammounmedaziz <kammounmohamedaziz@esprit.tn>
2025-06-12 03:24:39 +0100

    profile component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  { useState } from 'react';
 import "./index.css";
-import Navbar from "./Components/Navbar";
+// import Navbar from "./Components/Navbar";
 import WelcomeScreen from "./Pages/WelcomeScreen";
 import AnimatedBackground from "./Components/Background";
-import Home from "./Pages/Home";
+// import Home from "./Pages/Home";
 import { AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
-import About from "./Pages/About";
+// import About from "./Pages/About";
 // import NotFound from "./Pages/404Page";
-import Footer from "./Components/Footer";
+// import Footer from "./Components/Footer";
+import ProfileCard from "./Components/ProfileCard";
+
 
 
 
@@ -24,11 +26,10 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
 
       {!showWelcome && (
         <>
-          <Navbar />
+
           <AnimatedBackground />
-          <Home />
-          <About/>
-          <Footer/>
+          <ProfileCard/>
+          
 
           
           </>
